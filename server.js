3d13b59ea81ccea921c64c389acea5a7fae5437f
cndnhere <98242648+cndnhere@users.noreply.github.com>
2024-01-08 10:28:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    Accept");
       next();
     });
 app.use(express.json());
