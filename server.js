801a1d8807c3f7535211a87940ed5cf5985aa024
cndnhere <98242648+cndnhere@users.noreply.github.com>
2024-01-08 10:23:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const itemRoutes = require('./routes/VideoPlayerInfoApi');
 
 
 const app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
-  next();
-});
+  app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use(express.json());
 
 // Connect to MongoDB
